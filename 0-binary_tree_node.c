061bddeb7bda62932a332ec44ad2139b96f31bc6
Kevin Yook <yook00627@gmail.com>
2018-07-02 14:29:02 -0700

    creating binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,15 +1,29 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
+ * binary_tree_node - creates a binary tree node
+ * @parent: input parent node address
+ * @value: input value of the node
+ * Return: node address of the binary tree or NULL
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node;
 
-
-
-
-
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	if (parent == NULL)
+	{
+		node->n = value;
+		node->parent = NULL;
+		node->left = NULL;
+		node->right = NULL;
+		return (node);
+	}
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
