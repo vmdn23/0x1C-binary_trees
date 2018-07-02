8fc8d5cd95d15e4a58fcd7ef9ae048407f823218
Kevin Yook <yook00627@gmail.com>
2018-07-02 13:02:01 -0700

    added more in library

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -28,6 +31,8 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
