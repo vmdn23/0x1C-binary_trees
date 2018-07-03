e4b833911988b38e53f989330acead83c5e8f551
vmdn23 <victormdnguyen88@gmail.com>
2018-07-03 18:15:32 +0000

    betty check

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,5 +14,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left != NULL && tree->right != NULL)
 		return(binary_tree_is_full(tree->left) &&
 		       binary_tree_is_full(tree->right));
-	return(0);
+	return (0);
 }
