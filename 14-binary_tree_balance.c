fb386aa8fe7e06c87beeb2b8ef9891873a44813c
Kevin Yook <yook00627@gmail.com>
2018-07-02 18:01:11 -0700

    change some of the methods to run

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t rs, ls;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 	ls = binary_tree_height(tree->left);
 	rs = binary_tree_height(tree->right);
@@ -29,13 +29,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL)
-		lh = 0;
-	else
-		lh = binary_tree_height(tree->left) + 1;
-	if (tree->right == NULL)
-		rh = 0;
-	else
-		rh = binary_tree_height(tree->right) + 1;
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
 	return (lh - rh);
 }
