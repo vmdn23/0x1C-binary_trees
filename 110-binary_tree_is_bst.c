2a8de3e659bf423149f108dbf0032bb1d3883a8c
Kevin Yook <yook00627@gmail.com>
2018-07-05 13:57:34 -0700

    finding if the tree is BST

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: the input root address
+ * @min: minimum value of int
+ * @max: maximum value of int
+ * Return: 1 if true 0 if false
+ */
+int is_bst(const binary_tree_t *tree, int min, int max)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+	return ((is_bst(tree->left, min, tree->n)) &&
+		(is_bst(tree->right, tree->n, max)));
+}
+
+
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: the input root address
+ * Return: 1 if true 0 if false
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_bst(tree, INT_MIN, INT_MAX));
+}
